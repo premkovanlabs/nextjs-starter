d8b96c7614eefb405a31c878788ad3e8fc67204d
Premkumar Krishnamoorthi <premkumar@kovanlabs.com>
2024-06-10 13:17:46 +0530

    my first changes

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/reset.css';
 import '../styles/main.css';
 
 // This default export is required in a new `pages/_app.js` file.
+// changed on 10th June 2024
 export default function MyApp({ Component, pageProps }) {
   return (
     <div>
